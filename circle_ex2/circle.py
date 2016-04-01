f3cf54bb477bfa494a0d4f56cfa8c004e84e138a
Shawn O'Neil <oneilsh@gmail.com>
2016-04-01 15:09:38 -0700

    Post class Apr 1

--- circle_ex2/circle.py
+++ circle_ex2/circle.py
@@ -4,8 +4,18 @@ class Circle:
         self.pos = pos_vec
         self.d = d
         self.speed = speed_vec
-        self.acceleration = PVector(0, 0.5)
+        ## Acceleration is 0 by default
+        self.acceleration = PVector(0, 0)
+        ## mass is associated with diameter (perhaps should relate to area ;) )
+        self.mass = self.d/100
         
+        
+    ## f = ma, equivalently a = f/m
+    def apply_force(self, force_vec):
+        self.acceleration = self.acceleration + force_vec/self.mass
+
+    def reset_acceleration(self):
+        self.acceleration = self.acceleration * 0
 
     def draw(self):
         fill(150, 150, 150, 150)
--- circle_ex2/controller.py
+++ circle_ex2/controller.py
@@ -15,8 +15,27 @@ class Controller:
     
     def draw(self):
         background(0, 0, 0)
-
+        gravity = PVector(0, 0.5)
+        wind = PVector(0.2, 0)
+        
         for circle in self.circles:
+            if mousePressed:
+                #circle.apply_force(wind) # we could apply a wind force if we want
+                
+                ## gravitational attraction to the mouse...
+                mousepos = PVector(mouseX, mouseY)
+                tomouse = mousepos - circle.pos
+                disttomouse = tomouse.mag()
+                tomouse.normalize()
+                circle.apply_force(500*tomouse/(disttomouse**2))
+                
+            if keyPressed:
+                # gravity as a force scales with the mass of the object
+                # (and then is cancelled inside! by a = f/m!)                    
+                circle.apply_force(gravity * circle.mass)
+                
             circle.draw()
             circle.move()
-
+            
+            # the forces, applied from the outside, shall not accumulate over time
+            circle.reset_acceleration()
