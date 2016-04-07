45ea7ff51ed97d5f08f196dc9b548bf8c8f3cc66
Shawn O'Neil <oneilsh@gmail.com>
2016-04-07 11:37:44 -0700

    End of class Apr 6

--- agent_ex/agent.py
+++ agent_ex/agent.py
@@ -3,13 +3,21 @@ class Agent:
 
     def __init__(self, pos_vec, speed_vec, max_speed):
         self.pos = pos_vec
-        self.speed = speed_vec
+        self.speed = speed_vec 
         self.max_speed = max_speed
         
         # Acceleration is 0 by default
         self.acceleration = PVector(0, 0)
         # boring mass
         self.mass = 1.0
+        
+    def seek(self, target_vec):
+        desiredspeed = target_vec - self.pos
+        desiredspeed.normalize()
+        desiredspeed = desiredspeed * self.max_speed
+        
+        steerforce = desiredspeed - self.speed
+        self.apply_force(steerforce)
         
     # f = ma, equivalently a = f/m
     def apply_force(self, force_vec):
@@ -21,7 +29,24 @@ class Agent:
     def draw(self):
         fill(150, 150, 150, 150)
         stroke(0, 0, 0, 100)
-        ellipse(self.pos.x, self.pos.y, 10, 10)
+        #ellipse(self.pos.x, self.pos.y, 10, 10)
+        #rect(self.pos.x - 15, self.pos.y - 5, 30, 10)
+        #line(self.pos.x, self.pos.y, self.pos.x + 40, self.pos.y)
+        pushMatrix()
+        translate(self.pos.x, self.pos.y)
+        angle = atan2(self.speed.y, self.speed.x)
+        rotate(angle)
+        rect(-15, -5, 30, 10)
+        pushMatrix()
+        rotate(-1*PI/8)
+        line(0, 0, 40, 0)
+        popMatrix()
+        pushMatrix()
+        rotate(PI/8)
+        line(0, 0, 40, 0)
+        popMatrix()
+        line(0, 0, 40, 0)
+        popMatrix()
     
     def move(self):
         self.speed = self.speed + self.acceleration
--- agent_ex/controller.py
+++ agent_ex/controller.py
@@ -5,18 +5,23 @@ class Controller:
 
     def __init__(self):
         self.agents = list()
-        for i in range(0, 10):
+        for i in range(0, 10): 
             rspx = (random.random() - 0.5)*2
             rspy = (random.random() - 0.5)*2
-            rmaxsp = random.random() * 2
+            rmaxsp = random.random() * 4
             newagent = Agent(PVector(width/2, height/2), PVector(rspx, rspy), rmaxsp)
             self.agents.append(newagent)
 
     def draw(self):
         background(230, 230, 230)
+        mousepos = PVector(mouseX, mouseY)
         for agent in self.agents:
             agent.draw()
+            
+            if mousePressed:
+                agent.seek(mousepos)
+
             agent.move()
-            
-            
             agent.reset_acceleration()
+            
+            
